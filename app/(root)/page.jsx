54a516a773a275a69a0b33ba4e63f4e4f2c3f731
bolro <bolro@sm1l0.local>
2025-05-29 16:40:41 +0800

    init

--- app/(root)/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <section className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-3xl font-bold">This is Home.</h1>
-    </section>
-  );
-}
